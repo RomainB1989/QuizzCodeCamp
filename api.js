8702b945c72070b4e95fca55c15c4eaefffefba5
romainB <romain.jimmy.bergout@gmail.com>
2025-01-29 17:04:58 +0100

    Modif 29-01-25

--- api.js
+++ api.js
@@ -1,9 +1,6 @@
-const apiTest = document.querySelector('div');
-
-const compte = async()=> { 
+const apiFetch = async(url, method)=> { 
     try{
-        const rawData = await fetch('https://quizz.adrardev.fr/api/question/all');
-        console.log(rawData);
+        const rawData = await fetch(url, {method: method});
     
         if (!rawData.ok || rawData.status !== 200) {
             console.error("Erreur lors de la récupération des données : ", rawData.statusText);
@@ -11,14 +8,19 @@ const compte = async()=> {
         }
 
         const transformedData = await rawData.json();
-        console.log(transformedData);
-
-        let para = document.createElement('p');
-        para.innerText = transformedData[0].title;
-        apiTest.append(para);
+        return(transformedData);
+        // let para = document.createElement('p');
+        // para.innerText = transformedData[0].title;
+        // apiTest.append(para);
     }
-        catch (error) {
-            console.error("Erreur lors de l'appel à l'API : ", error);
-        }
+    catch (error) {
+        console.error("Erreur lors de l'appel à l'API : ", error);
+    }
 }
-compte();
+
+let arrayQuestion =  await apiFetch('https://quizz.adrardev.fr/api/question/all', "GET");
+let listUsers = await apiFetch('https://quizz.adrardev.fr/api/users', "GET");
+let listCategories = await apiFetch('https://quizz.adrardev.fr/api/category/all', "GET");
+console.log(arrayQuestion);
+console.log(listUsers);
+console.log(listCategories);
