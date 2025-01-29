7585613419aab37457dbfe9fc12bf3a316e02f7f
romainB <romain.jimmy.bergout@gmail.com>
2025-01-29 10:28:26 +0100

    Modif 29-01-25

--- menu.js
+++ menu.js
@@ -1,9 +1,6 @@
 const iconAccount = document.querySelector(".icon-account");
 const boxAccount = document.querySelector(".box-account");
 
-console.log(iconAccount, boxAccount);
-
 iconAccount.addEventListener('click', function() {
-    console.log(iconAccount, boxAccount);
     boxAccount.classList.toggle('active');
 });
